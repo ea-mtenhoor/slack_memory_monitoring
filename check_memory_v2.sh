aac99afb925027ca4f9ef91570d34b159abd0e64
ea-mtenhoor <42071768+ea-mtenhoor@users.noreply.github.com>
2020-06-03 11:54:31 -0500

    Add missing function get_top_users

--- check_memory_v2.sh
+++ check_memory_v2.sh
@@ -6,6 +6,10 @@ freeMem="$(free -g | grep Mem | awk '{print $3}')"
 percentMem=`echo "result = ($freeMem/$totalMem)*100; scale=0; result / 1" | bc -l`
 . config
 
+function get_top_users () {
+        topUsers=$(smem -urHk --columns="user pss" | head -5)
+}
+
 #State: okay=1  warning=2  critical=3
 if [ $percentMem -lt $warningPoint ]; then currentState=1; fi
 if [ $percentMem -ge $warningPoint ]; then currentState=2; fi
@@ -26,10 +30,12 @@ if [ $currentState -gt $previousState ]; then
         case "$currentState" in
                 2)
                         echo $currentState > previousState
+			get_top_users
                         curl -X POST -H 'Content-type: application/json' --data '{"attachments": [{"text":"WARNING! Memory usage high! '"$percentMem%"' <!here>","color":"warning"},{"text": " ", "fields": [{"title": "Top 5 Users", "value": "```'"${topUsers}"'```", "short": "true"}]}]}' $slackUrl > /dev/null 2>&1
                         ;;
                 3)
                         echo $currentState > previousState
+			get_top_users
                         curl -X POST -H 'Content-type: application/json' --data '{"attachments": [{"text":"CRITICAL! Memory usage very high! '"$percentMem%"' <!here>","color":"danger"},{"text": " ", "fields": [{"title": "Top 5 Users", "value": "```'"${topUsers}"'```", "short": "true"}]}]}' $slackUrl > /dev/null 2>&1
                         ;;
         esac
